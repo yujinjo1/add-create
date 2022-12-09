7dbb68f32021cb1f3b51a516a0cd4c3c8aec76c5
yujinjo1 <117976783+yujinjo1@users.noreply.github.com>
2022-12-09 16:55:54 +0900

    시도해봄.

--- sketch.js
+++ sketch.js
@@ -1,35 +1,137 @@
-let button; //센서 허용을 위한 버튼
-let permission = false; //허가를 위한 변수 (기본은 허가 안남)
+let button;
+let permission = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //윈도우 사이즈로 displayWidth displayHeight가능
-
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
-    // iOS 13 보다 높은 OS인 경우
-    background(255, 0, 0); //붉은 배경
-    button = createButton("Click to iOS Sensor"); //센서 허용을 위한 버튼
-    button.mousePressed(iosAccess); //버튼누르면 iosAccess 함수로
-  } else {
-    // non iOS 13+ 가 아닌 다른 OS 예: iOS저버전, 안드로이드, 윈도우, 맥
-    background(0, 255, 0); //녹색배경
-    text("is not a ios", 100, 100); //글자로 100, 100 위치에 is not a ios 라고 씀
-  }
+ createCanvas(windowWidth, windowHeight);
+  
+  
+ 
+ if(typeof DeviceMotionEvent.requsetPermission === 'function'){
+   background(255, 0, 0);
+   button = createButton("Click to iOS Sensor");
+   button.mousePressed(iosAccess);
+ } else {
+   background(0, 255, 0);
+   text("is not a ios", 100, 100)
+ }
 }
 
-function iosAccess() {  //버튼 누르면 동작하는 함수(그냥 따라하기)
-  DeviceOrientationEvent.requestPermission()
-    .then((response) => {
-      if (response === "granted") { //granted를 받으면
-        permission = true; //허가가 true (허가남)
-      }
-    })
-    .catch(console.error);
+function iosAccess() {
+ DeviceOrientationEvent.requestPermission().then((response) => {
+   if (response === "granted") {
+     permission = true;
+   }
+ })
+ .catch(console.error);
 }
 
 function draw() {
-  if (!permission) return; //허가 안나면 리턴
-  background(255, 255, 255); //흰색배경
-  textSize(72); //텍스트 픽셀 크기 72
-  text(rotationX, 100, 100); //텍스트 는 X회전값이고, 100 100 위치에 글자로 표기
+ //아이폰
+ if (!permission) return;
+ background(255, 255, 255);
+ text(72);
+ text(rotationX, 100, 100);
+}
+
+
+
+
+var wave;
+
+var playing = false;
+function setup() {
+createCanvas(720, 256);
+wave = new p5.Oscillator();
+wave.setType('sine');
+wave.start();
+wave.freq(440);
+wave.amp(0);
+button = createButton('play/pause');
+button.mousePressed(toggle);
+}
+function draw(){
+}
+
+
+function toggle() {
+if (!playing) {
+  wave.start();
+wave.amp(0.5, 1);
+playing = true;
+} else {
+wave.amp(0, 1);
+playing = false;
+}
+}
+//가속도색상
+let r, g, b;
+function setup() {
+createCanvas(displayWidth, displayHeight);
+r = random(50, 255);
+g = random(0, 200);
+b = random(50, 255);
+}
+function draw() {
+background(r, g, b);
+console.Log('draw’);
+}
+function deviceMoved() {
+r = map(accelerationX, -90, 90, 100, 175);
+g = map(accelerationY, -90, 90, 100, 200);
+b = map(accelerationZ, -90, 90, 100, 200);
+}
+  //가속도상자
+  
+  function setup() {
+createCanvas(displayWidth, displayHeight, WEBGL);
+}
+function draw() {
+background(250);
+normalMaterial();
+rotateX(accelerationX * 0.01);
+rotateY(accelerationY * 0.01);
+box(100, 100, 100);
+}
+  
+  //흔들기
+  
+  let balls = [];
+let threshold = 30;
+let accChangeX = 0;
+let accChangeY = 0;
+let accChangeT = 0;
+function setup() {
+createCanvas(displayWidth, displayHeight);
+for (let i = 0; i < 20; i++) {
+balls.push(new Ball());
+}
+}
+  function draw() {
+background(0);
+for (let i = 0; i < balls.length; i++) {
+balls[i].move(); balls[i].display();
+}
+checkForShake();
+}
+  
+  
+  //아이폰
+  
+let button;
+let permission=false;
+  
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  
+  
+ if (typeof DeviceMotionEvent. requestPermission === "function") {
+//최신 10s인 경우
+background(255, 0, 0); //붉은 배경
+button e createButton(rclick to ios Sensor"): 1/센서 허용을 위한 버튼
+but ton. mousePressed(iosAccess); //버튼누르면 10SAccess 함수로
+} else {
+//최신 10S가 아닌 다른 oS 예: 10s저버전, 안드로이드, 윈도우, 맥
+background(0,255, 0); //녹색배경
+text("is not a ios", 100, 100); //글자로 100, 100 위치에 is not a ios 라고 씀
+}}
 }
